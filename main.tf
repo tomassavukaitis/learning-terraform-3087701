dabce2fd97757db7c82b5f0c83075e183e6ffff1
tomassavukaitis <tomas.savukaitis@gmail.com>
2025-04-11 11:49:54 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -47,6 +47,41 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  # Security Group
+  security_groups = module.blog_sg.security_group_id
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "aws_instance.blog.id"
+    }
+  }
+
+http_tcp_listeners = [
+  {
+    port               = 80
+    protocol           = "HTTP"
+    target_group_index = 0
+  }
+
+]
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Example"
+  }
+}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
