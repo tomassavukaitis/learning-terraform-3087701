f655c6cec2cdf5214460e677a0798d410427c5d1
Tomas <tomas.savukaitis@gmail.com>
2025-04-16 12:00:23 +0300

    attempt 4

--- qa/main.tf
+++ qa/main.tf
@@ -2,13 +2,13 @@ module "qa" {
     source = "../modules/blog"
 
 
-environment "qa" {
+environment "qa" = {
     name           = "qa"
     network_prefix = "10.1"
 }
 
-asg_min_size = 1
-asg_max_size = 1
+asg_min = 1
+asg_max = 1
 
-
+# random comment
 }
